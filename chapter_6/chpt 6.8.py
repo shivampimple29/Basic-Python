81312f6bc3ef3f0844a881e9c794f486fb2ec3a8
Shivam S. Pimple <shivampimple29@gmail.com>
2025-08-26 05:37:57 +0530

    added chpt 6.7.py , chpt 6.8.py and chpt 6.9.py

---
+++ chapter_6/chpt 6.8.py
@@ -0,0 +1,10 @@
+#Q3
+
+age=int(input("Enter your age :"))
+if age>18 :
+ if age>60:
+  print("Senior Citizen Benefits!")
+ else:
+  print("Eligible to vote.")
+else:
+ print("Not eligible to vote.")
---
+++ chapter_6/chpt 6.9.py
@@ -0,0 +1,14 @@
+#Q4
+
+n=int(input("Enter a number:"))
+if n>0 :
+ print("It's a positive number.")
+elif n<0 :
+ print("It's a negative number.")
+else:
+ print("it's an odd number.")
+
+if n%2==0 :
+ print("It's an even number.")
+else:
+ print("it's an odd number.")
