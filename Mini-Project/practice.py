aa9eebfaa66db81e4fcea2e64b7ce99bdd7ec407
Shivam S. Pimple <shivampimple29@gmail.com>
2025-09-09 22:17:55 +0530

    Added mini-project

---
+++ Mini-Project/practice.py
@@ -0,0 +1,14 @@
+row=3
+for i in range(1,row+1):
+    print(' '*(row-i)+'*'*i+'*'*(i-1))
+print('---------------------------------')
+row=3
+for i in range(1,row+1):
+    print('*'*i)
+print('---------------------------------')
+row=3
+for i in range(1,row+1):
+    print('*',end=' ')
+    print('*')
+
+   
